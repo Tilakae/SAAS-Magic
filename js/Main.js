c5ccd0ddeefe7af9b880c0dc9b6cf72f1c4ded07
Tilakae <neupanetilak100@gmail.com>
2022-01-15 20:31:56 +0545

    SAAS Magic

---
+++ js/Main.js
@@ -0,0 +1,11 @@
+ /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="#body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });(jQuery);
